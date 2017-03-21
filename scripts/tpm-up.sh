5ac687256fe44412006398cb520ca488ada8f671
David J Gilhooley <djg5@princeton.edu>
2017-03-21 13:09:12 -0400

    CBMC: Moved files to toplevel folder
    I no longer have files in the _vboot_reference directory,
      which is much better for tracking purposes.

--- scripts/tpm-up.sh
+++ scripts/tpm-up.sh
@@ -1,4 +1,4 @@
-swtpm cuse -n tpm0 --tpmstate dir=/tmp
+swtpm cuse -n tpm0 --tpmstate dir=/tmp --log file=/tmp/vtpm.log,level=20
 swtpm_ioctl -i /dev/tpm0
 #TPM_DEVICE=/dev/tpm0 swtpm_bios
 #tcsd -f
