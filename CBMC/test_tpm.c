a9fbb9b4b5cb5166fdd8a13d4b6074eee47c8744
David J Gilhooley <djg5@princeton.edu>
2017-04-22 16:59:09 -0400

    CBMC: Added Test TPM file

---
+++ CBMC/test_tpm.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+int main(void) {
+    uint32_t req_size = nondet_int() % 256; 
+    uint32_t resp_size = nondet_int() % 256; 
+
+    uint8_t *request = malloc(req_size);
+    uint8_t *response = malloc(resp_size);
+
+    uint8_t *resp_len;
+
+	uint8_t indigest[20];
+	uint8_t outdigest[20];
+
+    for (int i = 0; i < 20; i++)
+        indigest[i] = i;
+
+    tpm_init();
+    VbExTpmSendReceive(request, req_size, response, resp_len);
+//    TlclExtend(0, &indigest, &outdigest); 
+}
